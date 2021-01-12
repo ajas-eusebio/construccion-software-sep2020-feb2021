5c754253bc20523ded1d9f7ae914687ec08a86e8
alexisRosaldo <77130670+alexisRosaldo@users.noreply.github.com>
2021-01-12 09:45:55 -0600

    Update BankTextController.java
    Requerimiento 3: La llave de la búsqueda deberá de ser "LastName y FirstName" unidos por un guión bajo" Ejemplo: Garcilazo_Juan. Mandar un mensaje al usuario si no se usa el guión bajo.

--- BankProject/src/bank/controller/BankTextController.java
+++ BankProject/src/bank/controller/BankTextController.java
@@ -85,24 +85,29 @@ public class BankTextController {
     }
 
     public String containsUnderscore(String key) {
-        String laCadena = key;
-        int contadorMay = 0;
-        for (int i = 0; i < laCadena.length(); i++) {
-            if (Character.isUpperCase(laCadena.charAt(i))) {
-                contadorMay++;
+        if (key.contains("_")) {
+            String laCadena = key;
+            int contadorMay = 0;
+            for (int i = 0; i < laCadena.length(); i++) {
+                if (Character.isUpperCase(laCadena.charAt(i))) {
+                    contadorMay++;
+                }
+                if (contadorMay >= 2 && laCadena.charAt(i - 1) == '_' && Character.isUpperCase(laCadena.charAt(i))) {
+                    key = key.replaceFirst("_", "");
+                    String cadena1 = key;
+                    String cadena2 = key;
+                    String cadenaParte1 = cadena1.substring(0, i - 1);
+                    //System.out.println(cadena1);
+                    String cadenaParte2 = cadena2.substring(i - 1, laCadena.length() - 1);
+                    //System.out.println(cadena2);
+                    key = cadenaParte2 + cadenaParte1;
+                    return key;
+                }
             }
-            if (contadorMay >= 2 && laCadena.charAt(i - 1) == '_' && Character.isUpperCase(laCadena.charAt(i))) {
-                key = key.replaceFirst("_", "");
-                String cadena1 = key;
-                String cadena2 = key;
-                String cadenaParte1 = cadena1.substring(0, i - 1);
-                //System.out.println(cadena1);
-                String cadenaParte2 = cadena2.substring(i-1, laCadena.length()-1);
-                //System.out.println(cadena2);
-                key = cadenaParte2 + cadenaParte1;
-                return key;
-            }
+        } else {
+            JOptionPane.showMessageDialog(null, "La contraseña no posee el guión bajo (_)");
         }
         return "";
+    }
     }
 }
